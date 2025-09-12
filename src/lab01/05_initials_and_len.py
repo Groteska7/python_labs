00667a6f5d868f3f2a124a56257c0b641e4e54b8
Никита Жуков <groteska7@inbox.ru>
2025-09-12 12:38:32 +0300

    ЛОХ

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,3 +1,4 @@
 name,fam,otch=map(str,input("ФИО: ").split())
 print(f"ФИО: {name[0].upper()+fam[0].upper()+otch[0].upper()}.")
 print(f'Длина (символов): {len(name)+len(fam)+len(otch)+2}')
+ПАГВАРПИПЛОВАРЛЛЛЛЛЛПЛОРЛОВАРАВПКШРПУКПРШВЩРАПАВРПВАПРВА
