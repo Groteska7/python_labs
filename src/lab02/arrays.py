caccad7cf1f88ca1ff89bb60b47470d06db07411
Никита Жуков <groteska7@inbox.ru>
2025-09-23 16:35:47 +0300

    lab02_ex1

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -1,15 +1,17 @@
-def min_max(IN_string):
-    work_mas=[]
-    if len(IN_string)==0:
-        print("ValueError")
-    if all([str(x) in "0123456789" for x in IN_string.split()]):
-        for x in IN_string.split():
-            if x.find("."):
-                work_mas.append(float(x))
-            else:
-                work_mas.append(int(x))
+def min_max(input_data: list[int,float]) -> tuple[float | int, float | int]:
+    if input_data==[]:
+        return "ValueError"
+    #if all([str(x) in "0123456789" for x in IN_string.split()]):
+    #    for x in IN_string.split():
+    #        if x.find("."):
+    #            work_mas.append(float(x))
+    #        else:
+    #            work_mas.append(int(x))
     else:
-        print("TypeError")
-    return work_mas
-IN_string=input("строкой через пробел: ")
-print(min_max(IN_string))
+        return input_data.sort()
+
+def unique_sorted(nums: list[float | int]) -> list[float | int]:
+    return sorted(set(nums))
+
+print(min_max(input().split()))
+print(unique_sorted(input().split()))
