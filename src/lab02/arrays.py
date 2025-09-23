78151e36b1f86581abefb7f4ef49562bf358f020
unknown <groteska7@inbox.ru>
2025-09-23 20:59:08 +0300

    lab02_ex1_B+C+PyThOn.txt_Face

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -1,4 +1,6 @@
 from typing import Union
+import json
+import ast
 
 def min_max(input_data: list[int|float]) -> list[int|float]:
     #print(input_data)
@@ -13,9 +15,23 @@ def min_max(input_data: list[int|float]) -> list[int|float]:
     else:
         return min(input_data),max(input_data)
 
-
 def unique_sorted(nums: list[float | int]) -> list[float | int]:
     return sorted(set(nums))
 
-#print(min_max([int(float(x)) if float(x)%1==0  else float(x) for x in input().split()]))
-print(unique_sorted([int(float(x)) if float(x)%1==0  else float(x) for x in input().split()]))
+def flatten(mat: list[list | tuple]) -> list:
+    answ=[]
+    for i in mat:
+        for j in i:
+            if str(j) in "0123456789":
+                answ.append(j)
+            else:
+                return "TypeError"    
+    return answ
+
+print("-------------------------------->",min_max(json.loads(input("min_max: "))))
+# print(min_max([int(float(x)) if float(x)%1==0  else float(x) for x in input().split()]))
+
+print("-------------------------------->",unique_sorted(json.loads(input("nique_sorted: "))))
+# print(unique_sorted([int(float(x)) if float(x)%1==0  else float(x) for x in input().split()]))
+
+print("-------------------------------->",flatten(ast.literal_eval(input("flatten: "))))
