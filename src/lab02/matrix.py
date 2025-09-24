b08d4fdb710bfc38804df512a497f6f2e26c9b36
Никита Жуков <groteska7@inbox.ru>
2025-09-24 15:00:17 +0300

    lab02_matrix_pA+pB

---
+++ src/lab02/matrix.py
@@ -0,0 +1,32 @@
+import ast
+import json
+from typing import Union
+
+def transpose(mat: list[list[float | int]]) -> list[list|int]:
+    remat=[]
+    reStr=[]
+    a=len(mat[0])
+    if not(all(len(mat[x])==a for x in range(len(mat)))):
+        return "ValueError"
+    for k in range(len(mat[0])):
+        for i in mat:
+            # print(i[k])
+            reStr.append(i[k])
+        remat.append(reStr)
+        reStr=[]
+    # print(reStr)
+    return remat
+
+def row_sums(mat: list[list[float | int]]) -> list[float|int]:
+    a=len(mat[0])
+    if not(all(len(mat[x])==a for x in range(len(mat)))):
+        return "ValueError"
+    string_sum=[]
+    for string in mat:
+        string_sum.append([sum(string)])
+    return string_sum
+
+
+# print(transpose(json.loads(input("transpose: "))))
+
+print(row_sums(json.loads(input("row_sums: "))))
