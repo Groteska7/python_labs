44c09d1e8bd0c879797ee3b9532a544b0bb586a1
Никита Жуков <groteska7@inbox.ru>
2025-10-09 12:17:02 +0300

    text_stats+last_ex_NO_README

--- src/lab03/text/normalize.py
+++ src/lab03/text/normalize.py
@@ -1,14 +1,14 @@
 def normalize(text: str, casefold: bool = True, yo2e: bool = True) -> str:
     lvl_1=""
     answ=""
-    print("----->",text)
+    # print("----->",text)
     if yo2e:
         text=text.replace("ё","е").replace("Ё","Е")
     text=text.replace("\\t"," ").replace("\\r"," ").replace("\\n"," ")
     for i in text.split():
         lvl_1+=str(i)+" "
     lvl_1=lvl_1[:-1]
-    print("--------->",lvl_1)
+    # print("--------->",lvl_1)
     if casefold:
         for x in lvl_1:
             answ+=x.lower()
--- src/lab03/text_stats.py
+++ src/lab03/text_stats.py
@@ -1,15 +1,35 @@
 import sys
 from text.count_freq import top_n
 from text.count_freq import count_freq
+from text.normalize import normalize
 
 
-def text_stats(inp: str) -> str:
+def text_stats(inp: str, flag: bool) -> str:
     print(f'Всего слов: {len(inp.split())}')
     print(f'Уникальных слов: {len(set(inp.split()))}')
-    print("Топ 5:")
-    for i in top_n(count_freq(inp.split())):
-        print(f'{i[0]}: {i[1]}')
+    if flag==True:
+        par_1=max([len(x) for x in inp.split()])
+        print(par_1)
+        # print(par_1)
+        # print(top_n(count_freq(inp.lower().split()))[0][0])
+        if par_1>5:
+            print("слово"," "*abs(par_1-5),"|","частота")
+        elif par_1<5:
+            print("слово","|","частота")
+            par_1=5
+        print("-"*(par_1+abs(par_1-5)+8))
+        for i in top_n(count_freq(inp.lower().split())):
+            print(f'{i[0]} {" "*(par_1-len(i[0]))} | {i[1]}')
+    elif flag==False:
+        print("Топ 5:")
+        x=0
+        while x<5:
+            i=top_n(count_freq(inp.split()))[x]
+            print(f'{i[0]}: {i[1]}')
+            x+1
 
 # text_stats(sys.stdin.read().split())
+print("введите True/False чтобы включить/выключить табличный режим")
+key=bool(input())
 for line in sys.stdin:
-    text_stats(line)
+    text_stats(normalize(line),key)
