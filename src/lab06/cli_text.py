43a00678be3893ea1092bf435a030e968530d160
unknown <groteska7@inbox.ru>
2025-11-20 04:09:36 +0300

    lab06{cli_text.py}

--- src/lab06/cli_text.py
+++ src/lab06/cli_text.py
@@ -1,22 +1,80 @@
+
 import argparse
+import sys
+from pathlib import Path
+from lib import make_obs
+
+PROJECT_ROOT = Path(__file__).parent.parent.parent
+sys.path.insert(0, str(PROJECT_ROOT))
+
+from src.lab05.lib import r_csv, r_json
+from src.lab03.text.count_freq import top_n, count_freq_f
+from src.lab03.text.tokenize import tokenize_f
+
+PROJECT_ROOT = Path(__file__).parent.parent.parent
+sys.path.insert(0, str(PROJECT_ROOT))
 
 def main():
-    parser = argparse.ArgumentParser(description="CLI‑утилиты лабораторной №6")
+    parser = argparse.ArgumentParser(description="CLI-утилиты лабораторной №6")
     subparsers = parser.add_subparsers(dest="command")
 
     # подкоманда cat
     cat_parser = subparsers.add_parser("cat", help="Вывести содержимое файла")
-    cat_parser.add_argument("--input", required=True)
+    cat_parser.add_argument("-i", "--input", dest="input_file", required=True, help="Путь входного файла")
     cat_parser.add_argument("-n", action="store_true", help="Нумеровать строки")
 
     # подкоманда stats
     stats_parser = subparsers.add_parser("stats", help="Частоты слов")
-    stats_parser.add_argument("--input", required=True)
-    stats_parser.add_argument("--top", type=int, default=5)
+    stats_parser.add_argument("-i", "--input", dest="input_file", required=True, help="Путь входного файла")
+    stats_parser.add_argument("--top", dest="top_n", type=int, default=5, help="вывести топ-X слов по частоте")
 
     args = parser.parse_args()
 
     if args.command == "cat":
-        """ Реализация команды cat """
+        path=Path(args.input_file)
+        if not path.is_absolute():
+            path = PROJECT_ROOT / path
+        if not path.exists():
+            raise FileNotFoundError("Указанный файл не найден")
+        if path.suffix==".csv":
+            data=list(list(x.values()) for x in r_csv(path))
+        elif path.suffix==".json":
+            data=data=list(list(x.values()) for x in r_json(path))
+        else:
+            raise TypeError("Недопустимый формат файла")
+        
+        if args.n:
+            for i in range(len(data)):
+                print(i+1,", ".join(data[i]))
+        else:
+            for i in range(len(data)):
+                print(", ".join(data[i]))
+
+
     elif args.command == "stats":
-        """ Реализация команды stats """
+        path=Path(args.input_file)
+        if not path.is_absolute():
+            path = PROJECT_ROOT / path
+        if not path.exists():
+            raise FileNotFoundError("Указанный файл не найден")
+        
+        if path.suffix==".csv":
+            data=list(list(x.values()) for x in r_csv(path))
+        elif path.suffix==".json":
+            data=data=list(list(x.values()) for x in r_json(path))
+        elif path.suffix==".txt":
+            with open(path,"r",encoding="utf-8") as file:
+                data = file.read()
+        else:
+            raise TypeError("Недопустимый формат файла")
+
+        data=top_n(count_freq_f(tokenize_f(data)),args.top_n)
+        if len(data)<args.top_n:
+            n=len(data)
+        else:
+            n=args.top_n
+        for num in range(n):
+            print(f'{data[num][0]}: {data[num][1]}')
+
+if __name__ == "__main__":
+        main()
--- src/lab06/lib.py
+++ src/lab06/lib.py
@@ -7,4 +7,6 @@ def make_obs(path: Path|str) -> Path:
     path=Path(path)
     if not path.is_absolute:
         path = PROJECT_ROOT / path
+    # if not path.exists():
+    #     raise FileNotFoundError("Файл не найден")
     return path
